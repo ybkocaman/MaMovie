d152e54c615483edfcefd40e9729d2dee22fc648
Yusuf <ybkocaman@gmail.com>
2024-03-18 20:53:12 +0000

    Initial Commit

---
+++ MaMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaMovie
+//
+//  Created by Yusuf Burak on 18/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaMovie/MaMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaMovieApp.swift
+//  MaMovie
+//
+//  Created by Yusuf Burak on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
